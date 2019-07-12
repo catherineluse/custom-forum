f30a9ca2c6d51cbae04b3bde756409624657dca4
Catherine Luse <you@example.com>
2019-07-12 03:21:27 -0700

    Add new community button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
-import React, { Component } from 'react'
-import { withAuthenticator } from 'aws-amplify-react'
+import React, { Component } from "react";
+import { withAuthenticator } from "aws-amplify-react";
 
 class App extends Component {
-  render () {
-    return <div>App</div>
+  state = {
+    communities: [
+      {
+        id: 1,
+        name: "Test community",
+      },
+    ],
+  };
+
+  render() {
+    const { communities } = this.state;
+
+    return (
+      <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+        <h1 className="code f2-1">Forums</h1>
+        {/* Community form */}
+        <form className="mb3">
+          <input
+            type="text"
+            className="pa2 f4"
+            placeholder="Write your community name"
+          />
+          <button className="pa2 f4" type="submit">
+            New Community
+          </button>
+        </form>
+
+        {/* Community List */}
+        <div>
+          {communities.map(item => (
+            <div key={item.id} className="flex items-center">
+              <li className="list pa1 f3">{item.name}</li>
+              <button className="bg-transparent bn f4">
+                <span>&times;</span>
+              </button>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
-export default withAuthenticator(App)
+export default withAuthenticator(App);
